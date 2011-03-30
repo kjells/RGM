a962c4ad77e1444bd67b5e9e207e7aa2ce7651b8
Marian Sievers <marian.sievers@gmail.com>
2011-03-30 15:44:13 +0200

    Reorganzied structure

---
+++ src/src_file.rb
@@ -0,0 +1,91 @@
+#RGM, the RubyGoMake
+#Copyright (C) 2011  Marian Sievers, avancet
+#
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+class SrcFile
+  #Klassenvariablen
+  @@src_dir = "src"
+  @@bin_dir = "bin"
+  @@tmp_dir = "tmp"
+  @@trenner = "-" * 20
+
+  #Compiler = gccgo OR 8g&8l
+  @@compiler = "go"
+  @@GCCGO = "gccgo"
+  @@GO_COMPILE = "6g"
+  @@GO_LINK = "6l"
+
+  def initialize(filename, folder)
+    @filename = filename
+    @src_folder = folder + "/"
+    @src_name = @filename + ".go"
+    @link_name = @filename + ".l"
+    @output_name = @filename + ".o"
+  end
+  
+  def initialize(filename)
+    @filename = filename
+    @src_folder = ""
+    @src_name = @filename + ".go"
+    @link_name = @filename + ".l"
+    @output_name = @filename + ".o"
+  end
+
+  def compile
+    if @@compiler == "gccgo"
+      puts "Not yet implemented!"
+    else
+      befehl = "#{@@arch}g -I #{@@tmp_dir} -o #{@@tmp_dir}/#{@link_name} #{@@src_dir}/#{@src_folder}#{@src_name}"
+    end
+
+    puts befehl
+    system(befehl)
+    puts @@trenner
+  end
+
+  def link
+    if @@compiler == "gccgo"
+      puts "Not yet implemented!"
+    else
+      befehl = "#{@@arch}l -o #{@@bin_dir}/#{@output_name} #{@@tmp_dir}/#{@link_name}"
+    end
+    puts befehl
+    system(befehl)
+    puts @@trenner
+  end
+
+  def self.set_compiler(compiler)
+    @@compiler = name
+  end
+  
+  #6 = 6g/l, 8=8g/l
+  def self.set_arch(arch)
+    @@arch = arch
+  end
+
+  def list_vars
+    puts @filename
+    puts @src_name
+    puts @link_name
+    puts @output_name
+    puts @@trenner
+  end
+end
+
+test_file = SrcFile.new("test")
+SrcFile.set_compiler("go")
+SrcFile.set_arch("6")
+test_file.list_vars()
+test_file.compile()
+test_file.link()
---
+++ src/test_file.rb
@@ -0,0 +1,65 @@
+# RGM, the RubyGoMake
+# Copyright (C) 2011  Marian Sievers, avancet
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+<<<<<<< HEAD
+#require 'BasicMake/read_data'
+require "src_file"
+=======
+require 'BasicMake/read_data'
+>>>>>>> b75613855064fd2c8b2fb53f8f4ebc6aea77b371
+
+puts "Hello World"
+
+test = Array.new()
+
+t1 = {"compile" => "foo", "link" => "foo"}
+t2 = {"compile" => "bar", "link" => "bar", "move" => {"from" => "src", "to" => "test"}}
+t3 = {"test" => "bar"}
+
+puts "Start push hashes in Array"
+test.push(t1)
+test.push(t2)
+test.push(t3)
+
+puts "Finish push hashes in array, start writing in YAML-file"
+
+<<<<<<< HEAD
+#ReadData.write_YAML("test.yaml", test)
+#x = ReadData.read_YAML("test.yaml")
+
+#puts x
+#puts "----------------"
+#puts x[0]
+#puts x[1]
+#puts x[2]
+#puts x.length
+#puts "----------------"
+#y = x[0]
+#puts y.length
+##y.flatten
+#puts y
+#puts y.values_at(0,1)
+
+file = SrcFile.new("test")
+SrcFile.set_compiler("go")
+#ReadData.write_YAML("compile.yaml", file)
+file.list_vars()
+file.compile()
+file.link()
+=======
+ReadData.write_YAML("test.yaml", test)
+x = ReadData.read_YAML("test.yaml")
+puts x
+>>>>>>> b75613855064fd2c8b2fb53f8f4ebc6aea77b371
